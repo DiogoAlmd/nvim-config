3c42a6d640940bc856eb4df1d8e41c43e959d839
Diogo Almeida <diogo.meida@gmail.com>
2024-09-04 14:02:29 -0300

    fix: LSPs fixes and configurations

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,30 @@
--- lua/plugins/lsp.lua
-
 return {
-    { 'neovim/nvim-lspconfig', event = "BufReadPre" },
-    {
-      'williamboman/mason.nvim',
-      config = function()
-        require('mason').setup()
-      end
-    },
+  {
+    'neovim/nvim-lspconfig',
+    event = "BufReadPre",
+    config = function()
+      local lspconfig = require('lspconfig')
+
+      lspconfig.tsserver.setup({
+        on_attach = function(client, bufnr)
+          client.resolved_capabilities.document_formatting = false
+        end,
+        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+      })
+    end
+  },
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "tsserver" },
+      })
+    end
   }
-  
+}
